f83c1ce143219835b49d6460d5df04ee79474b54
IrinaZagaynova <ayyyyrin@gmail.com>
2020-04-14 22:05:01 +0300

    GeneratePrimeNumbers

---
+++ lab2/GeneratePrimeNumbers/GeneratePrimeNumbers/GeneratePrimeNumbers/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "GeneratePrimeNumbers.h"
+
+int main(int argc, char* argv[])
+{
+	int upperBound;
+	if (!ParseArgs(argc, argv, upperBound))
+	{
+		return 1;
+	}
+
+	std::set<int> primeNumbers = GeneratePrimeNumbersSet(upperBound);
+	for (auto number : primeNumbers)
+	{
+		std::cout << number << " ";
+	}
+
+	return 0;
+}
+
+
