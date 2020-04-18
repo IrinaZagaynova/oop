f54822670493416edec6d384f43dc87551fd569b
IrinaZagaynova <ayyyyrin@gmail.com>
2020-04-18 15:21:49 +0300

    Update DecodeString.cpp

--- lab2/HTMLDecode/HTMLDecode/DecodeString.cpp
+++ lab2/HTMLDecode/HTMLDecode/DecodeString.cpp
@@ -2,13 +2,17 @@
 #include <iostream>
 #include <vector>
 
+const int MAX_LENGTH_OF_HTML_ENTITY = 6;
+const int MIN_LENGTH_OF_HTML_ENTITY = 4;
+const int NUMBER_PAIRS_OF_HTML_ENTITIES = 5;
+
 struct PairOfEntities
 {
 	std::string decodedEntity;
-	std::string entityToReplace;
+	std::string entityToDecode;
 };
 
-std::vector<PairOfEntities> htmlEntities = 
+const std::vector<PairOfEntities> htmlEntities = 
 {
 	{ "&quot;", "\"" },
 	{ "&apos;", "'" },
@@ -17,28 +21,50 @@ std::vector<PairOfEntities> htmlEntities =
 	{ "&amp;", "&" }
 };
 
-std::string ReplaceEntitiesInString(const std::string& decodingStr, const std::string& decodedEntity, const std::string& entityToReplace)
+int FindPairOfEntitiesForDecoding(size_t position, const std::string& inputStr, int numberPairOfEntities)
 {
-	std::string resultStr = decodingStr;
-
-	size_t position = resultStr.find(decodedEntity);
-	while (position != std::string::npos)
+	std::string str = "";
+	for (size_t i = position; ((i < inputStr.length()) && (i < position + MAX_LENGTH_OF_HTML_ENTITY)); i++)
 	{
-		resultStr.replace(position, decodedEntity.size(), entityToReplace);
-		position = resultStr.find(decodedEntity, position);
+		str += inputStr[i];	
+		if (str.length() >= MIN_LENGTH_OF_HTML_ENTITY)
+		{
+			for (size_t j = 0; j < NUMBER_PAIRS_OF_HTML_ENTITIES; j++)
+			{
+				if (htmlEntities[j].decodedEntity == str)
+				{
+					numberPairOfEntities = j;
+				}
+			}
+		}
 	}
-
-	return resultStr;
+	return numberPairOfEntities;
 }
 
 std::string DecodeString(std::string const& inputStr)
 {
-	std::string decodingStr = inputStr;
+	std::string decodedStr = "";
+	size_t position = 0;
+	int numberPairOfEntities = -1;
 
-	for (PairOfEntities entity : htmlEntities)
+	while (position < inputStr.length())
 	{
-		decodingStr = ReplaceEntitiesInString(decodingStr, entity.decodedEntity, entity.entityToReplace);
+		numberPairOfEntities = -1;
+		if (inputStr[position] == '&')
+		{
+			numberPairOfEntities = FindPairOfEntitiesForDecoding(position, inputStr, numberPairOfEntities);
+		}
+		if (numberPairOfEntities >= 0)
+		{
+			decodedStr += htmlEntities[numberPairOfEntities].entityToDecode;
+			position += htmlEntities[numberPairOfEntities].decodedEntity.length();
+		}
+		else
+		{
+			decodedStr += inputStr[position];
+			position++;
+		}
 	}
 
-	return decodingStr;
+	return decodedStr;
 }
