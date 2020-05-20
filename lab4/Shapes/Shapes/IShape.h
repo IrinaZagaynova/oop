27c803a283a943913fa33a3b754b5d93cc7559cd
IrinaZagaynova <ayyyyrin@gmail.com>
2020-05-20 12:31:25 +0300

    Update IShape.h

--- lab4/Shapes/Shapes/IShape.h
+++ lab4/Shapes/Shapes/IShape.h
@@ -6,9 +6,9 @@
 class IShape
 {
 public:
-	double virtual GetArea()const=0;
-	double virtual GetPerimeter()const=0;
-	std::string virtual ToString()const=0;
-	uint32_t virtual GetOutlineColor()const=0;
+	double virtual GetArea()const = 0;
+	double virtual GetPerimeter()const = 0;
+	std::string virtual ToString()const = 0;
+	uint32_t virtual GetOutlineColor()const = 0;
 	virtual ~IShape() {};
 };
