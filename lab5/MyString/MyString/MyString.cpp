48289ad81664ef308ce941a440126924ee685f21
IrinaZagaynova <ayyyyrin@gmail.com>
2020-06-09 13:25:16 +0300

    Added MyString

---
+++ lab5/MyString/MyString/MyString.cpp
@@ -0,0 +1,163 @@
+﻿#include "stdafx.h"
+#include "MyString.h"
+#include <algorithm>
+
+CMyString::CMyString()
+{
+	m_pString = nullptr;
+	m_length = 0;
+}
+
+CMyString::CMyString(const char* pString)
+	: CMyString(pString, strlen(pString))
+{	
+}
+
+CMyString::CMyString(const char* pString, size_t length)
+{
+	m_length = length;
+	m_pString = new char[m_length + 1];
+	memcpy(m_pString, pString, m_length);
+	m_pString[m_length] = '\0';
+}
+
+CMyString::CMyString(CMyString const& other)
+	: CMyString(other.m_pString, other.m_length)
+{
+}
+
+CMyString::CMyString(CMyString&& other) noexcept
+	: CMyString(other)
+{
+	other.m_pString = nullptr;
+	other.m_length = 0;
+}
+
+CMyString::CMyString(const std::string& stlString)
+	: CMyString(stlString.c_str(), stlString.length())
+{
+}
+
+CMyString::~CMyString()
+{
+	delete[] m_pString;
+}
+
+size_t CMyString::GetLength() const
+{
+	return m_length;
+}
+
+const char* CMyString::GetStringData() const
+{
+	return m_pString;
+}
+
+CMyString CMyString::SubString(size_t start, size_t length) const
+{
+	if (start > m_length)
+	{
+		throw std::exception("Out of range");
+	}
+	
+	if (start + length > m_length)
+	{
+		length = m_length - start;
+	}
+	
+	return(CMyString(m_pString + start, length));
+}
+
+void CMyString::Clear()
+{
+	delete[] m_pString; 
+	m_length = 0;
+	m_pString = new char[1];
+	m_pString[0] = '\0';
+}
+
+CMyString const CMyString::operator=(const CMyString& other)
+{
+	if (this != &other)
+	{
+		delete[] m_pString;
+		m_length = other.m_length;
+		m_pString = new char[m_length + 1];
+		memcpy(m_pString, other.m_pString, m_length);
+		m_pString[m_length] = '\0';
+	}
+
+	return *this;
+}
+
+CMyString const operator+(const CMyString& str1, const CMyString& str2)
+{
+	CMyString result;
+	result.m_length = str1.GetLength() + str2.GetLength();
+	result.m_pString = new char[result.m_length + 1];
+
+	memcpy(result.m_pString, str1.m_pString, str1.m_length);
+	memcpy(result.m_pString + str1.m_length, str2.m_pString, str2.m_length);
+	result.m_pString[result.m_length] = '\0';
+
+	return result;
+}
+
+CMyString& CMyString::operator+=(const CMyString& str2)
+{
+	*this = *this + str2;
+	return *this;
+}
+
+bool CMyString::operator==(const CMyString& other) const
+{
+	return (m_length == other.m_length && strcmp(m_pString, other.m_pString) == 0);
+}
+
+bool CMyString::operator!=(const CMyString& other) const
+{
+	return !(*this == other);
+}
+
+bool CMyString::operator<(const CMyString& other) const
+{
+	for (size_t i = 0; i < std::min(m_length, other.GetLength()); i++)
+	{
+		if (this->m_pString[i] < other.m_pString[i])
+		{
+			return true;
+		}
+	}
+
+	if (m_length < other.GetLength())
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+bool CMyString::operator>(const CMyString& other) const
+{
+	return (!(*this < other) && *this != other);
+}
+
+bool CMyString::operator<=(const CMyString& other) const
+{
+	return (*this < other || *this == other);
+}
+
+bool CMyString::operator>=(const CMyString& other) const
+{
+	return (*this > other || *this == other);
+}
+
+char const CMyString::operator[](size_t position) const
+{
+	return m_pString[position];
+}
+
+char& CMyString::operator[](size_t position)
+{
+	return m_pString[position];
+}
---
+++ lab5/MyString/MyString/main.cpp
@@ -0,0 +1,10 @@
+// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "stdafx.h"
+#include "MyString.h"
+
+int main()
+{   
+    return 0;
+}
